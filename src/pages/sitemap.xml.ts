2e236349045d8696a98d004fcf9b38ead91a3071
Noah Baron <32229274+noahbaron91@users.noreply.github.com>
2024-09-17 08:44:58 -0700

    feat: features

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,24 +1,33 @@
 import type { APIRoute } from 'astro';
 
+const URLS_PER_SITEMAP = 5000;
+
 export const GET: APIRoute = async ({ locals }) => {
   const db = locals.runtime.env.DB;
 
-  // todo: handle more than 50,000 urls
-  // const { results } = await db.prepare('SELECT * FROM page').all();
+  const { count } = await db
+    .prepare('SELECT COUNT(*) as count from page')
+    .first();
 
-  // console.log({ posts });
+  const numberOfSitemaps = Math.ceil(count / URLS_PER_SITEMAP);
+
+  const now = new Date();
+  const currentTime = now.toISOString();
+
+  const sitemaps = Array.from({ length: numberOfSitemaps }).map(
+    (_, index) =>
+      `<sitemap>
+          <loc>https://robotsblogging.com/sitemaps/${index}.xml</loc>
+          <lastmod>${currentTime}</lastmod>
+      </sitemap>
+    `
+  );
 
   // todo: return from a bucket & generate that once a day
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <!-- Example URLs -->
-    <url>
-      <loc>https://robotsblogging.com</loc>
-      <lastmod>2023-09-16</lastmod>
-      <changefreq>monthly</changefreq>
-      <priority>1.0</priority>
-    </url>
-  </urlset>`;
+  <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${sitemaps}
+  </sitemapindex>`;
 
   return new Response(sitemap, {
     headers: {
---
+++ src/pages/sitemaps/[slug].xml.ts
@@ -0,0 +1,39 @@
+import type { APIRoute } from 'astro';
+
+const URLS_PER_SITEMAP = 5000;
+
+export const GET: APIRoute = async ({ request, params, locals }) => {
+  console.log(params.slug);
+
+  const slug = Number(params.slug);
+  const db = locals.runtime.env.DB;
+  const offset = slug * URLS_PER_SITEMAP;
+
+  const { results: urls } = await db
+    .prepare(`SELECT path FROM page LIMIT ?1 OFFSET ?2`)
+    .bind(URLS_PER_SITEMAP, offset)
+    .all();
+
+  const urlsSitemap = urls
+    .map(({ path }: { path: string }) => {
+      return `<url>
+                <loc>https://www.robotsblogging.com${path}</loc>
+                <lastmod>2024-09-10</lastmod>
+                <changefreq>daily</changefreq>
+                <priority>0.5</priority>
+            </url>`;
+    })
+    .join('\n');
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${urlsSitemap}
+        </urlset>
+    `;
+
+  return new Response(sitemap, {
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+  });
+};
